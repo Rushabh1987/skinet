3210513a1dbf94b40d5e2ed2e452a6660ab2da78
Rushabh Pawar <rushabhpawar820@gmail.com>
2024-11-30 14:14:46 +0530

    End of secion 4

---
+++ Core/Specifications/TypeListSpecification.cs
@@ -0,0 +1,14 @@
+using System;
+using Core.Entities;
+using Core.Specification;
+
+namespace Core.Specifications;
+
+public class TypeListSpecification : BaseSpecification<Product, string>
+{
+    public TypeListSpecification()
+    {
+        AddSelect(x => x.Type);
+        ApplyDistinct();
+    }
+}
