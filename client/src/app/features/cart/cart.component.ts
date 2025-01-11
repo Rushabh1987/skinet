835466b097c8f19664636ef2f0e5506d27f5d544
Rushabh Pawar <rushabhpawar820@gmail.com>
2025-01-11 10:23:54 +0530

    End of section 13

---
+++ client/src/app/features/cart/cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../core/services/cart.service';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { OrderSummeryComponent } from '../../shared/components/order-summery/order-summery.component';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [CartItemComponent, OrderSummeryComponent],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.scss',
+})
+export class CartComponent {
+  cartService = inject(CartService);
+}
